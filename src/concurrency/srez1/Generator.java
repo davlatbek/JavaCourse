375b3ac274d7c5de6d33e2f27baaf75caeba9b90
davlatbek <idavlatbek19@gmail.com>
2017-06-12 10:01:49 +0300

    added generator

---
+++ src/concurrency/srez1/Generator.java
@@ -0,0 +1,29 @@
+package concurrency.srez1;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by davlet on 6/12/17.
+ */
+public class Generator implements Runnable{
+    List<Integer> integerList;
+    public Generator(List<Integer> integers){
+        this.integerList = integers;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        try {
+            for (int i = 0; i < 100; i++){
+                synchronized (integerList){
+                    integerList.add(random.nextInt(100));
+                }
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
